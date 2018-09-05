3fb7f57392b51c45a102c04ea0eab20479a78ab7
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-05 12:06:36 -0300

    Tests refactor

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../App'
+import { shallow } from 'enzyme'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+let wrapped
+beforeEach(() => {
+  wrapped = shallow(<App />)
+})
 
 it('shows a comment box', () => {
-  const div = document.createElement('div')
+  expect(wrapped.find(CommentBox).length).toEqual(1)
+})
 
-  ReactDOM.render(<App />, div)
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
-  expect(div.innerHTML).toContain('textarea')
-  expect(div.innerHTML).toContain('Submit')
-  ReactDOM.unmountComponentAtNode(div)
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1)
 })
